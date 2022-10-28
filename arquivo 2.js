6493a5cad0794eb35b5db61b55eab1c055b7c385
fipacheco <filipesmpacheco@gmail.com>
2022-10-27 21:58:23 -0300

    arquivo novo

---
+++ arquivo 2.js
@@ -0,0 +1,34 @@
+function soma(){
+
+}
+
+function subtracao(){
+
+}
+
+function divisao(){
+
+
+}
+
+function multiplicacao(){
+
+dhiuddsh
+}
+
+function piso(){
+
+}
+
+function teto(){
+    hyh
+
+}
+
+function uepa(){
+
+}
+
+function dota(dota){
+    return "melhor que lol"
+}
